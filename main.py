e9e9b65d3b4d003fb4f65803ff7c40e722de1157
Tina Morrell <librariantinamorrell@gmail.com>
2022-04-25 04:21:17 +0000

    new storylines

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 when = ['A millenia ago', 'In another life', 'While stealing a body', 'After jumping to a new body','After cleaning the grime from the crime scene']
 name = ['Bright Kelidescope,', 'Mayhem Muriel,','Werewolf Ken,', 'Black Ivy,', 'Soul Winter,']
-residence = ['Barcelona','India', 'Germany', 'Venice', 'England']
-went = ['home', 'insane','seminar', 'school', '']
-happened = ['felt resolute about finding the killer.','checked on their pet Sphinx.', 'found a secret key.', 'solved a mystery of the shapeshifter.', 'left for Paris.']
-print(random.choice(when) + ', ' + random.choice(name) + ' who lived in ' + random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
+residence = ['the land of the Winter Queen','in the catacombs under Paris', 'at thet top of the belltower', 'the cemetary', 'England']
+went = ['home', 'insane','to meet other superheroes', 'to the home of their arch nemisis', 'the secret library room']
+happened = ['felt resolute about finding the killer.','checked on their pet Sphinx.', 'found a secret key.', 'solved the mystery of the shapeshifter.', 'left the planet.']
+print(random.choice(when) + ', ' + random.choice(name) + ' who lived ' + random.choice(residence) + ', went ' + random.choice(went) + ' and ' + random.choice(happened))
